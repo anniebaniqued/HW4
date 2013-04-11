db8510db93800e5405c2bdecb8d7a65f591848cf
Annie <anniebaniqued@gmail.com>
2013-04-11 12:18:06 -0400

    3a and 3b

--- Problem3.R
+++ Problem3.R
@@ -4,7 +4,7 @@ n1 <- 1/sqrt(2*pi*25)*exp(-(x-1)^2/(2*25))
 n2 <- 1/sqrt(2*pi*1)*exp(-(x+2)^2/(2*1))
 n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*2))
 y <- 0.2*n1 + 0.3*n2 + 0.5*n3
-plot(x,y,type="l",lwd=2,col="red",xlim=c(-25,25))
+plot(x,y,type="l",lwd=2,col="red",xlim=c(-25,25),main="Mixture of Gaussians PDF")
 
 
 ############ PROBLEM 3(b) Direct Sampling
@@ -16,7 +16,8 @@ second <- as.numeric(.2 <= U & U < .5)
 third <- as.numeric(.5 <= U & U < 1)
 y <- first*rnorm(nsim,mean=1,sd=5)+second*rnorm(nsim,mean=-2,sd=1)+third*rnorm(nsim,mean=3,sd=2)
 
-hist(y,breaks=50,main="500 samples drawn from the mixture of Gaussians",col="yellow", freq=FALSE,xlim=c(-25,25))
+hist(y,breaks=50,main="500 samples drawn from the mixture of Gaussians",col="lightblue", freq=FALSE,xlim=c(-25,25))
 
 
 ############ PROBLEM 3(c) Rejection sampling
+
