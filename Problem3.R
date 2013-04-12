7dc7fb287b1d88b3e035836dfd9780cf82b14bd8
Annie <anniebaniqued@gmail.com>
2013-04-12 01:45:46 -0400

    all files

--- Problem3.R
+++ Problem3.R
@@ -1,8 +1,8 @@
 ############ PROBLEM 3(a) Density Function
-x=seq(-40,40,length=40000)
+x=seq(-25,25,length=40000)
 n1 <- 1/sqrt(2*pi*25)*exp(-(x-1)^2/(2*25))
 n2 <- 1/sqrt(2*pi*1)*exp(-(x+2)^2/(2*1))
-n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*2))
+n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*4))
 p <- 0.2*n1 + 0.3*n2 + 0.5*n3
 plot(x,p,type="l",lwd=2,col="red",xlim=c(-25,25),main="Mixture of Gaussians PDF")
 
@@ -16,27 +16,27 @@ second <- as.numeric(.2 <= U & U < .5)
 third <- as.numeric(.5 <= U & U < 1)
 y <- first*rnorm(nsim,mean=1,sd=5)+second*rnorm(nsim,mean=-2,sd=1)+third*rnorm(nsim,mean=3,sd=2)
 
-hist(y,breaks=50,main="500 samples drawn from the mixture of Gaussians",col="lightblue", freq=FALSE,xlim=c(-25,25))
+hist(y,breaks=35,main="500 samples drawn from the mixture of Gaussians",col="lightblue", freq=FALSE,xlim=c(-25,25))
 
 
 ############ PROBLEM 3(c) Rejection sampling
 
 # Just comparing the 2 distributions
-plot(x,p,type="l",lwd=2,col="red",xlim=c(-25,25),main="Mixture of Gaussians PDF")
+plot(x,p,type="l",col="red",xlim=c(-25,25),ylim=c(0,0.20),main="Mixture of Gaussians PDF")
 
 q <- 2.75/sqrt(2*pi*30)*exp(-(x-1.5)^2/(2*30))
 par(new=TRUE)
 lines(x,q,col="blue")
 
 # Implementing the rejection sampling
-sample.x = rnorm(10000,mean=1.5,sd=sqrt(30))
+sample.x = rnorm(2000,mean=1.5,sd=sqrt(30))
 accept = c()
 
 for(i in 1:length(sample.x)){
  p <- 0
  n1 <- 1/sqrt(2*pi*25)*exp(-(sample.x[i]-1)^2/(2*25))
  n2 <- 1/sqrt(2*pi*1)*exp(-(sample.x[i]+2)^2/(2*1))
- n3 <- 1/sqrt(2*pi*4)*exp(-(sample.x[i]-3)^2/(2*2))
+ n3 <- 1/sqrt(2*pi*4)*exp(-(sample.x[i]-3)^2/(2*4))
  p <- 0.2*n1 + 0.3*n2 + 0.5*n3
  if(runif(1, min=0, max=2.75*dnorm(sample.x[i],mean=1.5,sd=sqrt(30))) <= p) {
  	accept[i] <- 'Yes'
@@ -74,32 +74,32 @@ hist(T[,1][T$accept=='Yes'], breaks = 50, freq = FALSE, main = 'Histogram of Acc
 x=seq(-40,40,length=40000)
 n1 <- 1/sqrt(2*pi*25)*exp(-(x-1)^2/(2*25))
 n2 <- 1/sqrt(2*pi*1)*exp(-(x+2)^2/(2*1))
-n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*2))
+n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*4))
 p <- 0.2*n1 + 0.3*n2 + 0.5*n3
 lines(x,p,type="l",col="red",xlim=c(-25,25),main="Mixture of Gaussians PDF")
 
 library(ggplot2)
-print(qplot(sample.x, data = T, geom = 'histogram', fill = accept, binwidth=0.5))
+print(qplot(sample.x[1:1366], data = T[1:1366,], geom = 'histogram', fill = accept, binwidth=0.5))
 
 
 ############ PROBLEM 3(d) Metropolis-Hastings
 
 x <- 4
 chain <- c(x)
-iter <- 10000
-sd_n <- 0.5
+iter <- 1500
+var <- 5.0
+sd_n <- sqrt(var)
 for (i in 1:iter){
-	x <- tail(chain,1)
-	
+
 	n1 <- 1/sqrt(2*pi*25)*exp(-(x-1)^2/(2*25))
 	n2 <- 1/sqrt(2*pi*1)*exp(-(x+2)^2/(2*1))
-	n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*2))
+	n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*4))
 	p_x <- 0.2*n1 + 0.3*n2 + 0.5*n3
 	
 	xprime = rnorm(1,mean=x,sd=sd_n)
 	n1 <- 1/sqrt(2*pi*25)*exp(-(xprime-1)^2/(2*25))
 	n2 <- 1/sqrt(2*pi*1)*exp(-(xprime+2)^2/(2*1))
-	n3 <- 1/sqrt(2*pi*4)*exp(-(xprime-3)^2/(2*2))
+	n3 <- 1/sqrt(2*pi*4)*exp(-(xprime-3)^2/(2*4))
 	p_xprime <- 0.2*n1 + 0.3*n2 + 0.5*n3
 	
 	prob <- min(1,(p_xprime/p_x))
@@ -107,34 +107,19 @@ for (i in 1:iter){
 	u <- runif(1)
 	if ( u < prob){
 		chain <- c(chain, xprime)
-	} else { 
-		chain <- c(chain, x)
-	}
+		x <- xprime
+	} 
 }
-hist(chain,50,freq=FALSE,main="Histogram of Accepted Values")
 
-proposalfunction <- function(param){
-    return(rnorm(3,mean = param, sd= c(0.1,0.5,0.3)))
-}
+burnIn = 1000
+hist(chain[-(1:burnIn)],40,freq=FALSE,main=sprintf("Histogram of Accepted Values for Var=%.2f",var),xlim=c(-25,25))
+
+x=seq(-40,40,length=40000)
+n1 <- 1/sqrt(2*pi*25)*exp(-(x-1)^2/(2*25))
+n2 <- 1/sqrt(2*pi*1)*exp(-(x+2)^2/(2*1))
+n3 <- 1/sqrt(2*pi*4)*exp(-(x-3)^2/(2*4))
+p <- 0.2*n1 + 0.3*n2 + 0.5*n3
+lines(x,p,type="l",col="red",xlim=c(-25,25),main="Mixture of Gaussians PDF")
  
-run_metropolis_MCMC <- function(startvalue, iterations){
-    chain = array(dim = c(iterations+1,3))
-    chain[1,] = startvalue
-    for (i in 1:iterations){
-        proposal = proposalfunction(chain[i,])
-        
-        probab = exp(posterior(proposal) - posterior(chain[i,]))
-        if (runif(1) < probab){
-            chain[i+1,] = proposal
-        }else{
-            chain[i+1,] = chain[i,]
-        }
-    }
-    return(chain)
-}
- 
-startvalue = c(4,0,10)
-chain = run_metropolis_MCMC(startvalue, 10000)
- 
-burnIn = 5000
-acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
+acceptance = length(chain)/iter
+acceptance
